db3ece41dcac2633931018d9dda91e1f844ac3fb
Harry Sanders <harry.sandersiv@yahoo.com>
2022-07-15 01:51:49 -0400

    added additional information to blog schema

--- backend/models/blog.js
+++ backend/models/blog.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
+const { Schema } = mongoose;
 
 const blogSchema = new Schema({
   title: { type: String, required: true },
-  description: { type: String, required: true },
+  body: { type: String, required: true },
   image: { type: String, required: false },
+  comments: [{ body: String, date: Date }],
+  date: { type: Date, default: Date.now, immutable: true },
+  hidden: Boolean,
+  meta: {
+    votes: Number,
+    favs: Number,
+  },
 });
 
 module.exports = mongoose.model('Blog', blogSchema);
